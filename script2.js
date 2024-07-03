89ce4bd43c4093fc91706a056f48f798c5d50ddf
PradnyaKulkarni2005 <kulkarnipradnya1245@gmail.com>
2024-07-03 12:57:45 +0530

    Javascript file

---
+++ script2.js
@@ -0,0 +1,4 @@
+document.getElementById("indiaBtn").addEventListener("click", function() {
+    var container = document.getElementById("containerDiv");
+    container.classList.toggle("popup");
+});
